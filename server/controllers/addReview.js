451cfe76ac7fc1910e276262cecfda808581217f
Daniel Shin <daniel990402@gmail.com>
2022-09-19 15:54:27 -0400

    add photos and characteristics

--- server/controllers/addReview.js
+++ server/controllers/addReview.js
@@ -3,6 +3,7 @@ const models = require('../models');
 module.exports = (req, res) => {
   const productId = req.params['0'];
   const data = req.body;
+  console.log(data);
   models.addReview(productId, data, (result) => {
     if (result) {
       res.sendStatus(201);
--- server/models/addReview.js
+++ server/models/addReview.js
@@ -1,45 +1,67 @@
 const pool = require('../db');
 
-module.exports = (productId, data, callback) => {
-  pool.connect((err, client, release) => {
-    if (data.recommend === 'true') {
-      data.recommend = true;
-    } else {
-      data.recommend = false;
-    }
-    var text = `
-      INSERT INTO Reviews (
-        product_id,
-        rating,
-        date,
-        summary,
-        body,
-        recommend,
-        reported,
-        reviewer_name,
-        reviewer_email,
-        response,
-        helpfulness
+module.exports = async (productId, data, callback) => {
+  const client = await pool.connect();
+  data.photos = JSON.parse(data.photos);
+  data.characteristics = JSON.parse(data.characteristics);
+  var reviewQuery = `
+    INSERT INTO Reviews (
+      product_id,
+      rating,
+      date,
+      summary,
+      body,
+      recommend,
+      reported,
+      reviewer_name,
+      reviewer_email,
+      response,
+      helpfulness
+    )
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+  `
+  var reviewValues = [
+    Number(productId),
+    Number(data.rating),
+    (new Date()).toISOString(),
+    data.summary,
+    data.body,
+    data.recommend,
+    false,
+    data.name,
+    data.email,
+    null,
+    0
+  ]
+  const reviewResult = await client.query(reviewQuery, reviewValues)
+  data.photos.forEach(async (photo) => {
+    var photoQuery = `
+      INSERT INTO photos (
+        review_id,
+        url
       )
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+      VALUES ((SELECT id FROM reviews ORDER BY id DESC LIMIT 1), $1)
     `
-    var values = [
-      Number(productId),
-      Number(data.rating),
-      (new Date()).toISOString(),
-      data.summary,
-      data.body,
-      data.recommend,
-      false,
-      data.name,
-      data.email,
-      null,
-      0
+    const photoValues = [
+      photo
     ]
-    client.query(text, values)
-      .then(result => {
-        release();
-        callback(result.rows)
-      })
+    await client.query(photoQuery, photoValues)
   })
+  console.log(data)
+  for (var key in data.characteristics) {
+    var charQuery = `
+      INSERT INTO char_reviews (
+        review_id,
+        characteristic_id,
+        value
+      )
+      VALUES ((SELECT id FROM reviews ORDER BY id DESC LIMIT 1), $1, $2)
+    `
+    var charValues = [
+      key,
+      data.characteristics[key]
+    ]
+    await client.query(charQuery, charValues)
+  }
+  callback(reviewResult)
 }
