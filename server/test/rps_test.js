a80ad2b8c91ad3b0ea921438382ce3c2405cecdb
Daniel Shin <daniel990402@gmail.com>
2022-09-20 15:20:02 -0400

    added tests for all routes

--- server/test/rps_test.js
+++ server/test/rps_test.js
@@ -13,6 +13,35 @@ export const options = {
   },
 };
 
-export default function () {
-  http.get('http://localhost:3000/reviews/37311/meta');
+// export default function () {
+//   var data = {
+//     rating: 3,
+//     summary: 'summary',
+//     body: 'body',
+//     recommend: true,
+//     name: 'name',
+//     email: 'email@email.com',
+//     photos: ["url.com", "url2.com"],
+//     characteristics: {
+//       '1': 2,
+//       '5': 3
+//     }
+//   }
+//   http.post('http://localhost:3000/reviews/37311', JSON.stringify(data), {
+//     headers: {
+//       'Content-Type': 'application/json'
+//     }
+//   });
+// }
+
+// export default function() {
+//   http.get('http://localhost:3000/reviews/37311/list?sort=relevant&page=1&count=4')
+// }
+
+// export default function() {
+//   http.put('http://localhost:3000/reviews/37311/helpful')
+// }
+
+export default function() {
+  http.put('http://localhost:3000/reviews/37311/report')
 }
