913a2b2343cc77fe73701f8ed32734fedcece19f
Daniel Shin <daniel990402@gmail.com>
2022-09-21 21:30:16 -0400

    added loader file

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const controllers = require('./controllers');
+const path = require('path');
 
 router.get('/reviews/*/list', controllers.getReviews);
 router.get('/reviews/*/meta', controllers.getReviewMeta);
 router.post('/reviews/*', controllers.addReview);
 router.put('/reviews/*/helpful/', controllers.markReviewHelpful);
 router.put('/reviews/*/report', controllers.reportReview);
+router.get('/loaderio-f27f4de69c553b32d8b5b67bbc71444e', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '../../loaderio-f27f4de69c553b32d8b5b67bbc71444e.txt'))
+})
 
 module.exports = router;
